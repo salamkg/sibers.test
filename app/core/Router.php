5900be0a4e5c56d6775953ef0b68b0a2fa4f59d7
Salamat <salam.kg@gmail.com>
2022-06-22 19:00:48 +0600

    Routing, 404 page

--- app/core/Router.php
+++ app/core/Router.php
@@ -6,17 +6,24 @@ use app\core\Request;
 
 class Router {
 
-    protected array $routes;
+    protected array $routes = [];
     public Request $request;
+    public Response $response;
 
-    public function __construct(Request $request)
+    public function __construct(Request $request, Response $response)
     {
         $this->request = $request;
+        $this->response = $response;
     }
 
     public function get($path, $callback)
     {
         $this->routes['get'][$path] = $callback;
+    }
+
+    public function post($path, $callback)
+    {
+        $this->routes['post'][$path] = $callback;
     }
 
     public function resolve()
@@ -26,10 +33,43 @@ class Router {
         $callback = $this->routes[$method][$path] ?? false;
 
         if ($callback === false) {
-            echo 'Not Found';
-            exit;
+            $this->response->setStatusCode(404);
+            return $this->render("404");
         }
-        echo call_user_func($callback);
+        if (is_string($callback)) {
+            return $this->render($callback);
+        }
+
+        return call_user_func($callback);
+    }
+
+    public function render($view)
+    {
+        $contentLayout = $this->contentLayout();
+        $viewContent = $this->renderView($view);
+        return str_replace('{{content}}', $viewContent, $contentLayout);
+    }
+
+    public function renderContent($viewContent)
+    {
+        $contentLayout = $this->contentLayout();
+        return str_replace('{{content}}', $viewContent, $contentLayout);
+    }
+
+    protected function contentLayout()
+    {
+        ob_start();
+        include_once __DIR__."/../views/layouts/main.php";
+        // include_once Application::$ROOT_DIR."/views/layouts/main.php";
+        return ob_get_clean();
+    }
+
+    protected function renderView($view)
+    {
+        ob_start();
+        // include_once Application::$ROOT_DIR."/views/$view.php";
+        include_once __DIR__."/../views/$view.php";
+        return ob_get_clean();
     }
 
 }
